f4d11c34ddb51b2394644a6545562f888467bcba
João Pedro H. Frediani <51144585+joaopedroherbefrediani@users.noreply.github.com>
2025-04-09 22:04:15 -0300

    Atualização install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ echo "🔧 Instalando yt-dlp..."
 # Baixa o yt-dlp (Linux) e dá permissão de execução
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
 chmod +x yt-dlp
+chmod +x install.sh
 
 echo "✅ yt-dlp instalado com sucesso!"
